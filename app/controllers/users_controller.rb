48262a06f571527ae2a5727537d4cb8e8cfef5cc
mitok1212 <mitokatayama1231003@gmail.com>
2024-09-26 18:13:55 +0900

    loginのコントローラー機能修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+ #user_touroku_syori
     def create
       @user = User.new(user_params)
       if @user.save
-        session[:user_id] = @user.id  # ユーザー登録後に自動でログイン
+        session[:user_id] = @user.id  #auto_login
         render json: { message: 'User created and logged in', user: @user }, status: :created
       else
         render json: @user.errors, status: :unprocessable_entity
@@ -14,5 +15,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username, :email, :password, :age, :weight, :height, :gender)
     end
-  end
-  #ユーザー登録関連の処理
+  
+end
