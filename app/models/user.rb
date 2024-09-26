9562a69439f221b30d855bc49b6b50ba80b1a8ae
panacota87 <hy2020.8787@gmail.com>
2024-09-26 19:09:09 +0900

    model-user add 式

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,9 +17,24 @@ class User < ApplicationRecord
     private
 
     def set_default_requirements
-      # 年齢や性別から基礎代謝や必要栄養素を計算
-      DailyRequirement.create(user: self, carbohydrates: 250, proteins: 50, fats: 70)
+      # 年齢、性別、体重、身長を基に1日の必要カロリーを計算
+      bmr = calculate_bmr(age, gender, weight, height)
+      
+      # 必要カロリーの配分（炭水化物50-65%, タンパク質13-20%, 脂質20-30%）
+      carbohydrates = (bmr * 0.58) / 4
+      proteins = (bmr * 0.17) / 4
+      fats = (bmr * 0.25) / 9
+    
+      DailyRequirement.create(user: self, carbohydrates: carbohydrates, proteins: proteins, fats: fats)
       NutrientDeficiency.create(user: self)
       Character.create(user: self)
+    end
+    
+    def calculate_bmr(age, gender, weight, height)
+      if gender == 'male'
+        88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)
+      else
+        447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)
+      end
     end
 end
