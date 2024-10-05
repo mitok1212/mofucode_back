1890d889ddb622b2344f8d7a5ee13f76a9358611
chi0317 <148053115+chi0317@users.noreply.github.com>
2024-10-06 03:01:20 +0900

    errorhandling3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,31 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
+  private
+
+  def authenticate_request
+    token = request.headers['Authorization']&.split(' ')&.last
+    if token.nil?
+      return render json: { error: 'Token not provided' }, status: :unauthorized
+    end
+
+    begin
+      decoded_token = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+      @current_user = User.find(decoded_token['user_id'])
+    rescue JWT::ExpiredSignature
+      render json: { error: 'Token has expired' }, status: :unauthorized
+    rescue JWT::DecodeError
+      render json: { error: 'Invalid token' }, status: :unauthorized
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'User not found' }, status: :unauthorized
+    end
+  end
+end
+
+
+
+
+"""class ApplicationController < ActionController::Base
   #protect_from_forgery with: :null_session #追加
   before_action :authenticate_request
 
@@ -13,4 +40,4 @@ class ApplicationController < ActionController::API
       render json: { error: 'Unauthorized' }, status: :unauthorized
     end
   end
-end
+end"""
