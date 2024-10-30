39e58e0f709aff1a7d15b5d66db5ef78efd92a56
Ishigami100 <yamahaya5151@icloud.com>
2024-10-31 00:52:31 +0900

    どうにかUserの登録まで直す

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,25 @@
 class UsersController < ApplicationController
-  #protect_from_forgery # 追記
- #skip_before_action :verify_authenticity_token, only: [:create] # CSRFskip
- #skip_before_action :authenticate_request, only: [:csrf_token]
- #skip_before_action :verify_authenticity_token
- #user-touroku
-    def create
-      @user = User.new(user_params)
-      if @user.save
-       # session[:user_id] = @user.id  #auto_login
-       token = generate_jwt_token(@user.id)  # Generate JWT token
-        render json: { message: 'User created and logged in', user: @user }, status: :created
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
-    end
+  skip_before_action :authenticate_request, only: [:create] # Ensure this is placed correctly
 
-    #CSRFtoken
-    def csrf_token
-      render json: { csrf_token: form_authenticity_token }
+  def create
+    p user_params
+    @user = User.new(user_params)
+    if @user.save
+      token = generate_jwt_token(@user.id)  # Generate JWT token
+      render json: { message: 'User created and logged in', user: @user, token: token }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:username, :email, :password, :age, :weight, :height, :gender)
-    end
-
-     # JWT token generation method
-  def generate_jwt_token(user_id)
-    JWT.encode({ user_id: user_id, exp: 24.hours.from_now.to_i }, Rails.application.secrets.secret_key_base)
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password_digest, :age, :weight, :height, :gender)
+  end
+
+  # JWT token generation method
+  def generate_jwt_token(user_id)
+    JWT.encode({ user_id: user_id, exp: 24.hours.from_now.to_i }, Rails.application.credentials.secret_key_base)
+  end
 end
